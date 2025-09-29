826a81fa40506a787079dee4302fda5bb17b62e7
Samfp18 <165073213+Samfp18@users.noreply.github.com>
2025-09-29 14:23:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -46,6 +46,4 @@ def contact():
     except Exception as e:
         return jsonify({'error': f'Erro ao enviar a mensagem: {str(e)}'}), 500
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+
